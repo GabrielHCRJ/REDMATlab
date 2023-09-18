664c48e61a984f3d245f7ff46b85c68b6d9cb882
Gabriel Domingues <124419150+GabrielHCRJ@users.noreply.github.com>
2023-09-18 09:31:20 -0300

    Ficha Trigonométrica
    Site com páginas seno, cosseno, tangente funcionais.
    
    Falta sec,cossec,cotg.
    Consertar as respostas
    Colocar texto de apresentação

---
+++ FichaTrigonometria/FunçãoCosseno/questions.js
@@ -0,0 +1,94 @@
+let questions = 
+[
+    {
+        numb: 1,
+        question: "Domínio e Imagem",
+        videoSrc: "Cos_Questao1.mp4",
+        solVideoSrc: "Cos_Solucao1.mp4",         
+        answer: "C. Dom: ℝ, Im: [-1,1]",
+        options: [
+            "A. Dom:(-1,1) , Im: [-&#960,&#960]",
+            "B. Dom: ℝ, Im: ℝ",
+            "C. Dom: ℝ, Im: [-1,1]",
+            "D. Dom: [-1,1], Im: ℝ "
+        ]
+    },
+    {
+        numb: 2,
+        videoSrc: "Cos_Questao2.mp4",
+        solVideoSrc: "Cos_Solucao2.mp4", 
+        question: "Cortes nos eixos",
+        answer: "A. Eixo x: (kπ ,0), Eixo y:(0,0)",
+        options: [
+            "A. Eixo x: (kπ ,0), Eixo y:(0,0)",
+            "B. Eixo x: (0,0) , Eixo y: (-1,1)",
+            "C. Eixo x: (0,k&#960) , Eixo y:(0,0) ",
+            "D. Eixo x: (0,0), Eixo y:(k&#960,0) "
+        ]
+    },
+    {
+        numb: 3,
+        question: "Simetria",
+        videoSrc:"Cos_Questao3.mp4",
+        solVideoSrc: "Cos_Solucao3.mp4", 
+        answer: "B. Função Par",
+        options: [
+            "A. Função Ímpar",
+            "B. Função Par",
+            "C. Função par e impar",
+            "D. Nem uma das duas"
+        ]
+    },
+    {
+        numb: 4,
+        solVideoSrc: "Cos_Solucao4.mp4",
+        question: "Periodicidade. A função se repete em um intervalo de:",
+        videoSrc:"Cos_Questao4.mp4",
+        answer: "A. 2π",
+        options: [
+            "A. 2π",
+            "B. 3&#960",
+            "C. &#960/2",
+            "D. 3&#960/2"
+        ]
+    },
+    {
+        numb: 5,
+        solVideoSrc: "Cos_Solucao5.mp4",
+        question: "Os pontos que a função muda de concavidade são da forma:",
+        videoSrc:"Cos_Questao5.mp4",
+        answer: "D. (nπ,0) para n ∈ Z",
+        options: [
+            "A. (nπ/2,0) para n ∈ Z",
+            "B. (0,nπ) para n ∈ Z",
+            "C. (nπ,nπ) para n ∈ Z",
+            "D. (nπ,0) para n ∈ Z"
+        ]
+    },
+    {
+        numb: 6,
+        solVideoSrc: "Cos_Solucao6.mp4",
+        question: "Reciproca",
+        videoSrc:"Cos_Questao6.mp4",
+        answer: "B. cossec(x)",
+        options: [
+            "A. cos(x)",
+            "B. cossec(x)",
+            "C. sec(x)",
+            "D. cotg(x)"
+        ]
+    },
+    {
+        numb: 7,
+        solVideoSrc: "Cos_Solucao7.mp4",
+        question: "Qual a função Inversa",
+        videoSrc:"Cos_Questao7.mp4",
+        answer: "D. arcsen(x)",
+        options: [
+            "A. arctg(x)",
+            "B. arccos(x)",
+            "C. cos(x)",
+            "D. arcsen(x)"
+        ]
+    }
+];
